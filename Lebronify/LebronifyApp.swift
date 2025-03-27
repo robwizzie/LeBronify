e8843fca04a8100c67a1ac268fcda1b494a8138f
robwizzie <robertwiscount@gmail.com>
2025-03-26 20:01:04 -0400

    Initial Commit

---
+++ Lebronify/LebronifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LebronifyApp.swift
+//  Lebronify
+//
+//  Created by Robert Wiscount on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct LebronifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LebronifyTests/LebronifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LebronifyTests.swift
+//  LebronifyTests
+//
+//  Created by Robert Wiscount on 3/26/25.
+//
+
+import Testing
+@testable import Lebronify
+
+struct LebronifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
